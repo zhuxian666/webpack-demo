cd7ced5c836f5c8ddb2900c3217a5c35e820e148
zhuxian <2303103304@qq.com>
2020-06-16 15:16:41 +0800

    2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports={
     mode:'development',
     entry:'./src/index.js',
     output:{
-        filename:'[index].[contenthash].js'
+        filename:'index.[contenthash].js'
+    },
+    plugins:[new HtmlWebpackPlugin({
+        title:'猪猪',
+        template:'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader']
+            },
+        ],
     }
 };
